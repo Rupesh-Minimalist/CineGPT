10a5bce9bd7240f9f6f7bf9879c080401c670eb7
Rupesh-minimalist <imrupeshsingh07@gmail.com>
2024-07-08 23:11:50 +0530

    Tweeks

--- src/customHooks/netlify/nowPlaying.js
+++ src/customHooks/netlify/nowPlaying.js
@@ -1,12 +1,11 @@
+
+
 const fetch = require('node-fetch');
+const { API_OPTIONS } = require('../../utils/constant');
 
 exports.handler = async function(event, context) {
   try {
-    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_tmdb_key}`,
-      },
-    });
+    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS );
     const data = await response.json();
     return {
       statusCode: 200,
--- src/customHooks/useNowPlayingMovies.js
+++ src/customHooks/useNowPlayingMovies.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { API_OPTIONS } from "../utils/constants";
+import { API_OPTIONS } from "../utils/constant";
 import { addNowPlayingMovies } from "../redux/movieSlice";
 
 const useNowPlayingMovies = () => {
@@ -17,7 +17,7 @@ const useNowPlayingMovies = () => {
     try {
       const response = await fetch('./netlify/functions/nowPlaying', API_OPTIONS);
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(`HTTPpppppppppp error! status: ${response.status}`);
       }
       const data = await response.json();
       const movies = data.results;
