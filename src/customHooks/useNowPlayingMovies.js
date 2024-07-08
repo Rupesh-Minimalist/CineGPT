90e1bf54c533f675bae0dcbaddbf96991d1ee756
Rupesh-minimalist <imrupeshsingh07@gmail.com>
2024-07-08 17:33:20 +0530

    Tweeks

--- src/customHooks/useNowPlayingMovies.js
+++ src/customHooks/useNowPlayingMovies.js
@@ -2,6 +2,7 @@ import { API_OPTIONS, CORSProxy } from "../utils/constant";
 import { useDispatch, useSelector } from "react-redux";
 import { addNowPlayingMovies } from "../redux/movieSlice";
 import { useEffect } from "react";
+// import nowPlaying from "./netlify/functions/nowPlaying"
 
 const useNowPlayingMovies = () => {
   const dispatch = useDispatch();
@@ -15,7 +16,7 @@ const useNowPlayingMovies = () => {
   async function fetchNowPlaying() {
    
       let response = await fetch(
-        CORSProxy+"https://api.themoviedb.org/3/movie/now_playing?page=1",
+        "./netlify/functions/nowPlaying",
         API_OPTIONS
       );
      
