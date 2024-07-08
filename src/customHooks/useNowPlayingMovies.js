56df23e28075029de4784e1a0232cffc1f5ee309
Rupesh-minimalist <imrupeshsingh07@gmail.com>
2024-07-08 17:42:11 +0530

    Tweeks

--- src/customHooks/useNowPlayingMovies.js
+++ src/customHooks/useNowPlayingMovies.js
@@ -16,7 +16,7 @@ const useNowPlayingMovies = () => {
   async function fetchNowPlaying() {
    
       let response = await fetch(
-        "./netlify/functions/nowPlaying",
+        CORSProxy+"https://api.themoviedb.org/3/movie/now_playing?page=1",
         API_OPTIONS
       );
      
